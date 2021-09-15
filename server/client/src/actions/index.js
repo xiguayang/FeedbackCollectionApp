60a702b10ece718b333374abb5455da79abd8ea2
xiguayang <yangzhangamber1990@gmail.com>
2021-09-14 23:24:10 -0700

    Setup Route, Adding CSS, Adding Header
    Setup Route in App.js by BrowserRouter Route;
    Adding Materialize CSS by Webpack module loader
    Adding Header Component changing Content by Fetching current user from backend API, using axios redux-thunk (Action Creator, reducer, change Header to classbased Component to using connect)

---
+++ server/client/src/actions/index.js
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+//make a AJAX request to backend Express API
+//have already set proxy in setupProxy.js add '/api'
+
+// Original Version:
+// export const fetchUser = () => {
+// 	return function (dispatch) {
+// 		axios.get('/api/current_user').then((res) => dispatch({ type: FETCH_USER, payload: res }));
+// 	};
+// };
+
+//Refactor step 1: the arrow function only return 1 so remove return
+// export const fetchUser = () =>
+// 	function (dispatch) {
+// 		axios.get('/api/current_user').then((res) => dispatch({ type: FETCH_USER, payload: res }));
+// 	};
+
+//Refactor Step 2: using => instead of function keyword
+// export const fetchUser = () => (dispatch) => {
+// 	axios.get('/api/current_user').then((res) => dispatch({ type: FETCH_USER, payload: res }));
+// };
+
+//Refactor Step 3: using async await instead of Promise
+export const fetchUser = () => async (dispatch) => {
+	const res = await axios.get('/api/current_user');
+	dispatch({ type: FETCH_USER, payload: res.data });
+};
--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,5 +1,28 @@
 import React from 'react';
-const App = () => {
-	return <div>Hi There</div>;
-};
-export default App;
+import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
+import Header from './Header';
+const Dashboard = () => <h2>Dashboard</h2>;
+const SurveyNew = () => <h2>SurveyNew</h2>;
+const Landing = () => <h2>Landing</h2>;
+class App extends React.Component {
+	componentDidMount() {
+		this.props.fetchUser();
+	}
+	render() {
+		return (
+			<div className="container">
+				<BrowserRouter>
+					<div>
+						<Header />
+						<Route exact={true} path="/" component={Landing} />
+						<Route exact path="/surveys" component={Dashboard} />
+						<Route path="/surveys/new" component={SurveyNew} />
+					</div>
+				</BrowserRouter>
+			</div>
+		);
+	}
+}
+export default connect(null, actions)(App);
