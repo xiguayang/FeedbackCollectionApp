55b21e50ee0a16a904189e7bfd53d88402ca250b
xiguayang <yangzhangamber1990@gmail.com>
2021-09-16 14:38:43 -0700

    using Heroku to run build for deployment for client side react app

--- server/index.js
+++ server/index.js
@@ -29,6 +29,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+	//Express will serve production assets
+	//like our mian.js file, or main.css file
+	app.use(express.static('client/build'));
+	//Express will serve up the index.html file
+	//if it doesent't recognize the route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 //tell NodeJS to listen to this port
 //using environment variable defined by Heroku
 const PORT = process.env.PORT || 5000;
